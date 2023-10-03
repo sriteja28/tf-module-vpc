5c742a0762c1e44f0b54cceea0cab2a6ecbfb1e3
Sriteja Kattamuru <131655915+sriteja28@users.noreply.github.com>
2023-10-03 14:06:29 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "main" {
-  cidr_block = var.cidr_block
+  cidr_block         = var.cidr_block
   enable_dns_support = true
 
   tags = merge({
@@ -11,22 +11,22 @@ resource "aws_vpc" "main" {
 module "subnets" {
   source = "./subnets"
 
-  for_each = var.subnets
-  cidr_block = each.value["cidr_block"]
+  for_each    = var.subnets
+  cidr_block  = each.value["cidr_block"]
   subnet_name = each.key
 
   vpc_id = aws_vpc.main.id
 
-  env = var.env
+  env  = var.env
   tags = var.tags
-  az = var.az
+  az   = var.az
 }
 
 
 resource "aws_vpc_peering_connection" "peer" {
-  peer_vpc_id   = aws_vpc.main.id
-  vpc_id        = var.default_vpc_id
-  auto_accept   = true
+  peer_vpc_id = aws_vpc.main.id
+  vpc_id      = var.default_vpc_id
+  auto_accept = true
 }
 
 resource "aws_internet_gateway" "igw" {
@@ -39,13 +39,13 @@ resource "aws_internet_gateway" "igw" {
 }
 
 resource "aws_route" "route-igw" {
-  route_table_id            = module.subnets["public"].route_table_ids
-  destination_cidr_block    = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.igw.id
+  route_table_id         = module.subnets["public"].route_table_ids
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
 }
 
 resource "aws_eip" "ngw" {
-  domain   = "vpc"
+  domain = "vpc"
 }
 
 
@@ -60,15 +60,15 @@ resource "aws_nat_gateway" "ngw" {
 }
 
 resource "aws_route" "route-ngw" {
-  count = length(local.private_route_table_ids)
-  route_table_id            = element(local.private_route_table_ids, count.index)
-  destination_cidr_block    = "0.0.0.0/0"
-  nat_gateway_id = aws_nat_gateway.ngw.id
+  count                  = length(local.private_route_table_ids)
+  route_table_id         = element(local.private_route_table_ids, count.index)
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.ngw.id
 }
 
 
 resource "aws_route" "peer-route" {
-  count = length(local.all_route_table_ids)
+  count                     = length(local.all_route_table_ids)
   route_table_id            = element(local.all_route_table_ids, count.index)
   destination_cidr_block    = "172.31.0.0/16"
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
